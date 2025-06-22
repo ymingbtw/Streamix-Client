1c8e84b4ba12112e5bff8eab36c3b0d37ef55936
Yming <ymingbytheway@gmail.com>
2025-06-22 13:27:40 +0700

    'a'

--- src/pages/components/Admin.jsx
+++ src/pages/components/Admin.jsx
@@ -15,15 +15,12 @@ export default function Admin({ children }) {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function authenticate() {
-      const res = await axios.get(
-        "https://api.ecnet.website/api/authenticate",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          withCredentials: true,
-        }
-      );
+      const res = await axios.get("https://api.ecnet.space/api/authenticate", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      });
       setRole(res.data.role);
       setIsAuthenticated(res.data.isAuthenticated);
       setToken(res.data.token);
